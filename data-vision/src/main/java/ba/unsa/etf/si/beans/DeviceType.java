754a83050cd6257b9dabd104df2603b1d9963ced
nkunalic2 <nkunalic@etf.unsa.ba>
2014-05-18 04:05:03 +0200

    DeviceType

---
+++ data-vision/src/main/java/ba/unsa/etf/si/beans/DeviceType.java
@@ -0,0 +1,19 @@
+package ba.unsa.etf.si.beans;
+
+public class DeviceType implements java.io.Serializable  {
+	long type_id;
+	public long getType_id() {
+		return type_id;
+	}
+	public void setType_id(long type_id) {
+		this.type_id = type_id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	String type;
+
+}
