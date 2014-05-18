3b7e8e90cb0373e340a15ac248cddd1a142b7ade
aomanovic <ao15155@etf.unsa.ba>
2014-05-18 03:31:59 +0200

    ispravka gresaka

--- data-vision/src/main/java/etf/si/projekat/data_vision/runnableClass.java
+++ data-vision/src/main/java/etf/si/projekat/data_vision/runnableClass.java
@@ -3,7 +3,7 @@ package etf.si.projekat.data_vision;
 public class runnableClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		WelcomeScreen s = new WelcomeScreen();
 		s.setVisible(true);
 	}
