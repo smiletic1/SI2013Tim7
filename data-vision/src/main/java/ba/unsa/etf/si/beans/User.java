1a30f01b29b5b228e6cf495e3f7ee04d51bab28e
mkiselica1 <mkiselica1@etf.unsa.ba>
2014-05-18 03:49:33 +0200

    Klasa User

---
+++ data-vision/src/main/java/ba/unsa/etf/si/beans/User.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.si.beans;
+
+public class User implements java.io.Serializable {
+	
+	long user_id;
+	String a;
+	public long getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	String name;
+	
+	public User() {}
+	
+
+}
