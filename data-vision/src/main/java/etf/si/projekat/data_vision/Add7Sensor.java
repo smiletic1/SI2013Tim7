7983afdaf6762839719c6b33965128f212bb0b1d
smiletic1 <smiletic1@etf.unsa.ba>
2014-05-20 22:51:29 +0200

    choice

--- data-vision/src/main/java/etf/si/projekat/data_vision/Add7Sensor.java
+++ data-vision/src/main/java/etf/si/projekat/data_vision/Add7Sensor.java
@@ -109,6 +109,14 @@ public class Add7Sensor extends JFrame {
 		choice_6.setBounds(131, 200, 120, 20);
 		contentPane.add(choice_6);
 		
+		choice.add(""); 
+		choice_1.add(""); 
+		choice_2.add("");
+		choice_3.add("");
+		choice_4.add("");
+		choice_5.add("");
+		choice_6.add("");
+		
 
 
 		
